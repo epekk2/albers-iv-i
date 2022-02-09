0a8332d6c82fa7d1b4379548b041769efdce480d
epekk2 <97483314+epekk2@users.noreply.github.com>
2022-02-09 16:47:50 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,6 @@
 
-let clrs = [
-'#E27F34', //top 0
-'#FEF254', //middle top 1
-'#A36C33', //middle strip 2
-'#042060', //middle bottom 3
-'#3778AE' //bottom 4
-]
+let clrs = ['#800000', '#FEF254', '#7f7c7e', 
+            '#7372f2', '#74f374']
 
 
 function setup() {
@@ -19,8 +14,8 @@ function draw() {
  canvasRect(clrs[0], height/2, 0, false);
  fill(clrs[2]); 
  rect(width * 1/3, height * 1/6, width * 1/3, height * 2/3); 
- canvasRect(clrs[1], height/6, height/3, true);
- canvasRect(clrs[3], height/6, height/2, true);
+ canvasRect(clrs[0], height/6, height/3, true);
+ canvasRect(clrs[4], height/6, height/2, true);
 
 
 }
